99ee348c21765873435c36a058e2dfa4b2b3b64e
Ahmad Solyman <a.bala1213@gmail.com>
2021-01-17 10:56:06 +0200

    Stable version

--- returnsCalculator/index.js
+++ returnsCalculator/index.js
@@ -2,21 +2,28 @@ let a = document.getElementById('a'),
     n = document.getElementById('n'),
     pre = document.getElementById('pre'),
     btn = document.getElementById('btn'),
-    show = document.getElementById('show'),
-    btn = document.getElementById('btn')
+    show = document.getElementById('show')
 
 
 function calc() {
-     let newA = parseInt(a.value),
-     newN = parseInt(n.value),
-     newPRE = parseInt(pre.value),
-     preee = newPRE / 100
 
-    let r = ((newA * preee) + newA) / newA,
-         resualt = Math.pow(r, newN) * newA
+        let newA = parseInt(a.value),
+            newN = parseInt(n.value),
+            newPRE = parseInt(pre.value),
+            preee = newPRE / 100,
+            r = ((newA * preee) + newA) / newA,
+            resualt = Math.pow(r, newN) * newA
+            endResualt = Math.round(resualt)
+    
+    if (isNaN(endResualt)) {
+        console.log('wrong')
+    } else 
+    { show.innerHTML = endResualt.toLocaleString()
+    }
 
-         rr = Math.round(resualt).toLocaleString()
-
-    show.innerHTML = rr
-
+        
+            
+    
 }
+
+
